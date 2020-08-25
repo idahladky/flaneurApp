d81319859128b7380f5c618b58c7dc9bc5fadda7
Ida <idahuladki@yahoo.com>
2020-08-25 15:36:47 -0400

    cities array added

--- app.js
+++ app.js
@@ -1,40 +1,103 @@
 $(() => {
 
-    // get string value from input
+    // array of cities
+    const cities = [
+        {
+            name: "New York, NY",
+            latitude: 40.730610,
+            longitude: -73.935242
+            
+        },
+        {
+            name: "Los Angeles, CA",
+            latitude: 34.052235,
+            longitude: -118.243683
+        },
+        {
+            name: "Chicago, IL",
+            latitude: 41.881832,
+            longitude: -87.623177
+        },
+        {
+            name: "Houston, TX",
+            latitude: 29.749907,
+            longitude: -95.358421
+        },
+        {
+            name: "Phoenix, AZ",
+            latitude: 33.448376,
+            longitude: -112.074036
+        },
+        {
+            name: "Philadelphia, PA",
+            latitude: 39.952583,
+            longitude: -75.165222
+        },
+        {
+            name: "Denver, CO",
+            latitude: 39.742043,
+            longitude: -104.991531
+        },
+        {
+            name: "Boston, MA",
+            latitude: 42.361145,
+            longitude: -71.057083
+        },
+        {
+            name: "Nashville, TN",
+            latitude: 36.174465,
+            longitude: -86.767960
+        },
+        {
+            name: "Seattle, WA",
+            latitude: 47.608013,
+            longitude: -122.335167
+        }
+    ]
 
-    $("form").on("submit", (event) => {
-        event.preventDefault()
-
-        let locationSearch = $("input").val()
-    
-   
-        // pass value into geolocation api to get longitute and latitude values
-
-        // $.ajax({
-        //     url: 
-        // }).then(
-        //     (data) => {
-        //         console.log(data)
-        //     },
-        //     () => {
-        //         console.log("bad request")
-        //     }
-    
-        // save longitude and latitude values into variables
-        let longitude = 0
-        let latitude = 0
-
-        // pass above variables into weather api call
-
-        // $.ajax({
-        //     url: 
-        // }).then(
-        //     (data) => {
-        //         console.log(data)
-        //     },
-        //     () => {
-        //         console.log("bad request")
-        //     }
-        // )
-    })
 })
+
+
+
+
+
+
+// the code I will use when I can use both API's
+// --------------------------
+// get string value from input
+
+// $("form").on("submit", (event) => {
+//     event.preventDefault()
+
+//     let locationSearch = $("input").val()
+
+
+    // pass value into geolocation api to get longitute and latitude values
+
+    // $.ajax({
+    //     url: 
+    // }).then(
+    //     (data) => {
+    //         console.log(data)
+    //     },
+    //     () => {
+    //         console.log("bad request")
+    //     }
+
+    // save longitude and latitude values into variables
+    // let longitude = 0
+    // let latitude = 0
+
+    // pass above variables into weather api call
+
+    // $.ajax({
+    //     url: 
+    // }).then(
+    //     (data) => {
+    //         console.log(data)
+    //     },
+    //     () => {
+    //         console.log("bad request")
+    //     }
+    // )
+// })
